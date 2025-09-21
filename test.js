5ff5664cbfb2e020710bdaee4206ac9a710ed990
Anna <email@email.com>
2025-09-21 17:45:49 +0300

    задание с рандомными числами

---
+++ test.js
@@ -0,0 +1,67 @@
+const t = 11
+
+console.log(t)
+
+// 1. Create a variable and check if it is undefined using an if block.
+let ton
+if(typeof ton === "undefined") {
+    console.log("yeeeeeeeeee");}
+else {console.log ("noooooooo");}
+
+
+
+// 2. Create a string variable and check if it is empty using an if block.
+let mon = "";
+if(mon ==="") 
+    {
+    console.log("yeeeeeeeeee");
+}
+else {console.log ("noooooooo");}
+
+
+// 3. Create a boolean variable and check 
+// if it is true or false using an if block.
+
+let oon = true;
+if(oon ===false) 
+    {
+    console.log("yeeeeeeeeee");
+}
+else {console.log ("noooooooo");}
+
+
+// 4. Create a variable with a string value and check if it matches another string using ===.
+let jon = "ron";
+if(jon ==="lon") 
+    {
+    console.log("yeeeeeeeeee");
+}
+else {console.log ("noooooooo");}
+
+
+
+// 5. Create a variable for age and write an if-else block to check if the person is 18 or older.
+let ion=30;
+if(ion ===30) 
+    {
+    console.log("yeeeeeeeeee");
+}
+else {console.log ("noooooooo");}
+
+
+
+let aon={
+    color: "red",
+    wheel: 4
+}
+aon.wheel= 5
+console.log(aon)
+
+let eon=aon
+eon.wheel=10
+console.log(eon)
+console.log(aon)
+
+const userList=['jonk','mike']
+
+console.log(userList.includes('mike'))
